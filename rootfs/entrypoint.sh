059da56e565994a2cdb4fe075b18391b586ee3a1
NEDIX <2658411+nedix@users.noreply.github.com>
2024-10-21 02:48:09 +0200

    Refactor s6-stage2-hook directory

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -60,5 +60,5 @@ ROOT_DIRECTORY="${ROOT_DIRECTORY%/}"
 # -------------------------------------------------------------------------------
 exec env -i \
     S6_CMD_WAIT_FOR_SERVICES_MAXTIME="$(( $STARTUP_TIMEOUT * 1000 ))" \
-    S6_STAGE2_HOOK=/sbin/s6-stage2-hook \
+    S6_STAGE2_HOOK=/usr/sbin/s6-stage2-hook \
     /init
